e8b51d6c5c94c2abebb5cdf62779e7c6eb4599c0
Atsushi Morimoto (74th) <site@74th.tech>
2021-10-26 11:02:43 +0900

    standard grpc

--- tasks.py
+++ tasks.py
@@ -8,6 +8,10 @@ def build_proto(c):
         proto/gisapp.proto""")
 
 @task
+def build_default_proto(c):
+    c.run(f"""protoc -I=. --python_out=. ./proto/gisapp.proto""")
+
+@task
 def apply_base(c):
     c.run(f"""kustomize build manifests/base | kubectl apply -f -""")
 
